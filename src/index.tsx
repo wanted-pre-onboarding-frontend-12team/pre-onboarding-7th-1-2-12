8e125829bb7b2c913e096c1f1c14cc9173c5dd57
hopak-e <a328028@gmail.com>
2022-10-30 20:36:38 +0900

    :ambulance: 화면 안나오던 에러 해결

--- src/index.tsx
+++ src/index.tsx
@@ -3,15 +3,12 @@ import ReactDOM from 'react-dom/client';
 import { ThemeProvider } from 'styled-components';
 import App from './App';
 import { GlobalStyle, theme } from './styles';
-import { IssueProvider } from './constants/IssueContext';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
 	<React.StrictMode>
 		<ThemeProvider theme={theme}>
-			<IssueProvider>
-				<GlobalStyle />
-				<App />
-			</IssueProvider>
+			<GlobalStyle />
+			<App />
 		</ThemeProvider>
 	</React.StrictMode>,
 );
