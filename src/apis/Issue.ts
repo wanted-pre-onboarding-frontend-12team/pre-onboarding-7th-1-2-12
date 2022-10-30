5ca36ff36840d68ff7ecef2777864ef616f797f4
hopak-e <a328028@gmail.com>
2022-10-30 16:53:33 +0900

    ✨ issue 목록 get 요청 함수 작성

---
+++ src/apis/Issue.ts
@@ -0,0 +1,8 @@
+import axios from 'axios';
+
+export const getIssues = async () => {
+	const res = await axios.get('https://api.github.com/repos/angular/angular-cli/issues?sort=comments&page=1&per_page=8', {
+		headers: { Authorization: `token ${import.meta.env.VITE_API_KEY}` },
+	});
+	return res.data;
+};
